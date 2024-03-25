8e0be2e2c9d51a8a28c18464d57980c474a2a44b
danilo9 <kalinindanilq7734@gmail.com>
2024-03-25 15:52:20 +0700

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ro2005.ru';
-export const SITE_DESCRIPTION = 'Портфолио - Калинин Данил Евгеньевич | Скилы, Проекты и Блог';
+export const SITE_TITLE = 'Калинин Данил';
+export const SITE_DESCRIPTION = 'Портфолио - Калинин Данил Евгеньевич | Скилы, Проекты и Карьера';
