0edc1479718ec0aeba02410decec22d347b98049
danilo9 <kalinindanilq7734@gmail.com>
2024-04-06 03:23:48 +0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ro2005.ru',
+	site: 'https://kalinind.ru',
 	integrations: [mdx(), sitemap()],
 });
