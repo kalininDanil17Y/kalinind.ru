a4a0f7f7be2d005d59d7ceede974e0a8f887dcee
danilo9 <kalinindanilq7734@gmail.com>
2024-08-11 04:56:35 +0800

    Add localizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://kalinind.ru',
 	integrations: [mdx(), sitemap()],
+	output: 'hybrid'
 });
---
+++ src/utils/localize.js
@@ -0,0 +1,40 @@
+export const getLocaleFromHeaders = (headers) => {
+    const acceptLanguage = headers.get('accept-language');
+
+    if (!acceptLanguage) return 'en'; // Язык по умолчанию
+
+    const languages = acceptLanguage.split(',').map(lang => lang.split(';')[0].trim());
+
+    if (languages.includes('ru')) {
+        return 'ru';
+    } else {
+        return 'en';
+    }
+};
+
+export const getLocaleTexts = async (locale) => {
+    try {
+        if (locale === 'ru') {
+            return await import('../locales/ru.json');
+        } else {
+            return await import('../locales/en.json');
+        }
+    } catch (error) {
+        console.error('Error loading localization file', error);
+        return null;
+    }
+};
+
+export const getLocale = (request) => {
+    // Попробуем получить язык из sessionStorage
+    const storedLang = request.headers.get('cookie')?.match(/locale=([^;]+)/)?.[1];
+
+    let lang;
+    if (storedLang) {
+        lang = storedLang;
+    } else {
+        lang = getLocaleFromHeaders(request.headers);
+    }
+
+    return lang === 'en' ? 'en' : 'ru';
+};
